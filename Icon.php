cc1f8a3d242b4df5f79406d726b030902454c561
cuileon <admin@yiichina.com>
2018-05-29 15:31:34 +0800

    add space option

--- Icon.php
+++ Icon.php
@@ -9,34 +9,34 @@ use yii\helpers\Html;
  */
 class Icon
 {
-    public static function show($name, $options = [])
+    public static function show($name, $options = [], $space = true)
     {
-        return static::fas($name, $options);
+        return static::fas($name, $options, $space);
     }
 
-    public static function fas($name, $options = [])
+    public static function fas($name, $options = [], $space = true)
     {
         FontAwesomeAsset::register(\Yii::$app->getView());
         Html::addCssClass($options, ['type' =>'fas', 'name' => 'fa-' . $name]);
-        return Html::tag('i', null, $options) . ' ';
+        return Html::tag('i', null, $options) . ($space ? ' ' : null);
     }
 
-    public static function far($name, $options = [])
+    public static function far($name, $options = [], $space = true)
     {
         FontAwesomeAsset::register(\Yii::$app->getView());
         Html::addCssClass($options, ['type' =>'far', 'name' => 'fa-' . $name]);
-        return Html::tag('i', null, $options) . ' ';
+        return Html::tag('i', null, $options) . ($space ? ' ' : null);
     }
 
-    public static function fal($name, $options = [])
+    public static function fal($name, $options = [], $space = true)
     {
-        return new FontAwesome('fal', $name, $options) . ' ';
+        return new FontAwesome('fal', $name, $options) . ($space ? ' ' : null);
     }
 
-    public static function fab($name, $options = [])
+    public static function fab($name, $options = [], $space = true)
     {
         FontAwesomeAsset::register(\Yii::$app->getView());
         Html::addCssClass($options, ['type' =>'fab', 'name' => 'fa-' . $name]);
-        return Html::tag('i', null, $options) . ' ';
+        return Html::tag('i', null, $options) . ($space ? ' ' : null);
     }
 }
